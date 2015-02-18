5b4e84c668cb892cdaf195053852be4fa2e63b87
Bryan Sieber <bryan_sieber@berkeley.edu>
2015-02-17 18:42:20 -0800

    Part 1: removed DESC

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,10 +8,10 @@ class MoviesController < ApplicationController
 
   def index
     if params[:sort_by_title] == 'true'
-      @movies =Movie.find(:all, :order => "title DESC")
+      @movies =Movie.find(:all, :order => "title")
       @sorted_by = "title"
     elsif params[:sort_by_date] == 'true'
-      @movies =Movie.find(:all, :order => "release_date DESC")
+      @movies =Movie.find(:all, :order => "release_date")
       @sorted_by = "release_date"
     else
       @movies = Movie.all
